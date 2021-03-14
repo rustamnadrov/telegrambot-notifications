56a232f57aa4a764e00a9849e076e5d8b93588d7
rustamnadrov <nadrov.email@gmail.com>
2021-03-14 21:45:58 +0300

    updates

--- request.js
+++ request.js
@@ -19,7 +19,7 @@ bot.onText(/\/launch/, (msg) => {
     for (let i = 0; i < servers.length; i++) {
       request(servers[i], function (error, response, body) {
         if (body == undefined) {
-          bot.sendMessage(msg.from.id,
+          bot.sendMessage(msg.chat.id,
             // Notification text
             `Server ${servers[i]} is down. Need do something`)
         };
@@ -29,7 +29,7 @@ bot.onText(/\/launch/, (msg) => {
 });
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.from.id,
+  bot.sendMessage(msg.chat.id,
     `Hello, ${msg.from.first_name}!
 1. Fill the array servers.
 2. Select interval.
